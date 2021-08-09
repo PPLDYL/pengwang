a8af1d5729176dfa572e9f3cd40714a209ecdb68
PPLDYL <57738345+PPLDYL@users.noreply.github.com>
2021-08-09 16:34:12 +0800

    Add files via upload

---
+++ src/settings.54c9d.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/2.fire",orientation:"",jsList:[],bundleVers:{internal:"1b082",main:"f747d"}};
